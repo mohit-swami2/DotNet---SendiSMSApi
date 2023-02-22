04bb79de4891632e6bf118d67f460f0a07356f04
_MohitSwami <106970399+mohit-swami2@users.noreply.github.com>
2023-02-22 15:07:22 +0530

    Add project files.

---
+++ Controllers/SMSComtroller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace SMSApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSComtroller : ControllerBase
+    {
+
+        private readonly ITwilioRestClient _client;
+        public SMSComtroller(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost("Api-Send-smd")]
+        public IActionResult SendSms(SmsMessage model)
+        {
+            var message = MessageResource.Create(
+                to: new PhoneNumber(model.To),
+                from: new PhoneNumber(model.From),
+                body: model.Message,
+                client: _client);
+            return Ok("Success" + message.Sid); 
+        }
+    }
+}
